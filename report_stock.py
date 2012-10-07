3e176ff5cd2dd7532fa76eb2e47b51dc11c44942
Sebastien LANGE <sebastien.lange@syleam.fr>
2012-10-07 14:00:15 +0400

    [IMP] Fix search view and review all view in report stock

--- report_stock.py
+++ report_stock.py
@@ -4,6 +4,7 @@
 #    wms module for OpenERP, This module allows to manage crossdocking in warehouses
 #    Copyright (C) 2011 SYLEAM (<http://www.syleam.fr/>)
 #              Christophe CHAUVET <christophe.chauvet@syleam.fr>
+#              Sebastien LANGE <sebastien.lange@syleam.fr>
 #
 #    This file is a part of wms
 #
@@ -25,82 +26,93 @@
 from osv import osv
 from osv import fields
 from tools.sql import drop_view_if_exists
+import decimal_precision as dp
 
 
-class ReportStockReal(osv.osv):
+class wms_report_stock_available(osv.osv):
     """
-    Display the stock available, per unit, production lot and tracking number
+    Display the stock available, per unit, production lot
     """
     _name = 'wms.report.stock.available'
     _description = 'Stock available'
     _auto = False
     _rec_name = 'product_id'
 
+    USAGE_SELECTION = [
+        ('supplier', 'Supplier Location'),
+        ('view', 'View'),
+        ('internal', 'Internal Location'),
+        ('customer', 'Customer Location'),
+        ('inventory', 'Inventory'),
+        ('procurement', 'Procurement'),
+        ('production', 'Production'),
+        ('transit', 'Transit Location for Inter-Companies Transfers')
+    ]
+
     _columns = {
         'product_id': fields.many2one('product.product', 'Product', readonly=True),
         'uom_id': fields.many2one('product.uom', 'UOM', readonly=True),
         'prodlot_id': fields.many2one('stock.production.lot', 'Production lot', readonly=True),
-        'tracking_id': fields.many2one('stock.tracking', 'Tracking', readonly=True),
         'location_id': fields.many2one('stock.location', 'Location', readonly=True),
         'warehouse_id': fields.many2one('stock.warehouse', 'Warehouse', readonly=True),
-        'qty': fields.float('Qty', readonly=True),
+        'product_qty': fields.float('Quantity', digits_compute=dp.get_precision('Product UoM'), readonly=True),
+        'usage': fields.related('location_id', 'usage', type='selection', selection=USAGE_SELECTION, string='Location Type', help="""* Supplier Location: Virtual location representing the source location for products coming from your suppliers
+                       \n* View: Virtual location used to create a hierarchical structures for your warehouse, aggregating its child locations ; can't directly contain products
+                       \n* Internal Location: Physical locations inside your own warehouses,
+                       \n* Customer Location: Virtual location representing the destination location for products sent to your customers
+                       \n* Inventory: Virtual location serving as counterpart for inventory operations used to correct stock levels (Physical inventories)
+                       \n* Procurement: Virtual location serving as temporary counterpart for procurement operations when the source (supplier or production) is not known yet. This location should be empty when the procurement scheduler has finished running.
+                       \n* Production: Virtual counterpart location for production operations: this location consumes the raw material and produces finished products
+                      """),
     }
 
     def init(self, cr):
         drop_view_if_exists(cr, 'wms_report_stock_available')
         cr.execute("""
-            CREATE OR REPLACE VIEW wms_report_stock_available AS (
-                SELECT max(id) AS id,
-                    (SELECT warehouse_id
-                     FROM   stock_location
-                     WHERE  id=report.location_id) AS warehouse_id,
-                    location_id,
-                    product_id,
-                    (SELECT
-                       product_template.uom_id
-                     FROM
-                       product_product,
-                       product_template
-                     WHERE
-                       product_product.product_tmpl_id = product_template.id AND
-                       product_product.id = report.product_id) AS uom_id,
-                    prodlot_id,
-                    tracking_id,
-                    sum(qty) AS qty
-                FROM (
-                    SELECT -max(sm.id) AS id,
-                        sm.location_id,
-                        sm.product_id,
-                        sm.prodlot_id,
-                        sm.tracking_id,
-                        -sum(sm.product_qty /uo.factor) AS qty
-                    FROM stock_move as sm
-                    LEFT JOIN stock_location sl
-                        ON (sl.id = sm.location_id)
-                    LEFT JOIN product_uom uo
-                        ON (uo.id=sm.product_uom)
-                    WHERE state = 'done' AND sm.location_id != sm.location_dest_id
-                    GROUP BY sm.location_id, sm.product_id, sm.product_uom, sm.prodlot_id, sm.tracking_id
-                    UNION ALL
-                    SELECT max(sm.id) AS id,
-                        sm.location_dest_id AS location_id,
-                        sm.product_id,
-                        sm.prodlot_id,
-                        sm.tracking_id,
-                        sum(sm.product_qty /uo.factor) AS qty
-                    FROM stock_move AS sm
-                    LEFT JOIN stock_location sl
-                        ON (sl.id = sm.location_dest_id)
-                    LEFT JOIN product_uom uo
-                        ON (uo.id=sm.product_uom)
-                    WHERE sm.state = 'done' AND sm.location_id != sm.location_dest_id
-                    GROUP BY sm.location_dest_id, sm.product_id, sm.product_uom, sm.prodlot_id, sm.tracking_id
-                ) AS report
-                GROUP BY location_id, product_id, prodlot_id, tracking_id
-                HAVING sum(qty) >0)
+                CREATE OR REPLACE VIEW wms_report_stock_available AS (
+                    WITH RECURSIVE location(id, name, parent_id, warehouse_id) AS (
+                                    select sw.lot_stock_id, ''::varchar, 0, sw.id
+                                    FROM   stock_warehouse sw
+                                    UNION
+                                    SELECT sl.id, sl.name, sl.location_id, sl.warehouse_id FROM stock_location sl, location
+                                    WHERE  sl.location_id = location.id)
+                    SELECT  max(id) AS id,
+                            (SELECT warehouse_id FROM stock_location WHERE id=report.location_id) AS warehouse_id,
+                            location_id,
+                            product_id,
+                            (SELECT product_template.uom_id FROM product_product, product_template WHERE product_product.product_tmpl_id = product_template.id AND product_product.id = report.product_id) AS uom_id,
+                            prodlot_id,
+                            sum(qty) AS product_qty
+                    FROM (
+                           SELECT   -max(sm.id) AS id,
+                                    sm.location_id,
+                                    sm.product_id,
+                                    sm.prodlot_id,
+                                    -sum(sm.product_qty /uo.factor) AS qty
+                           FROM stock_move as sm
+                           LEFT JOIN stock_location sl ON (sl.id = sm.location_id)
+                           LEFT JOIN product_uom uo ON (uo.id=sm.product_uom)
+                           WHERE state = 'done' AND sm.location_id != sm.location_dest_id
+                           GROUP BY sm.location_id, sm.product_id, sm.product_uom, sm.prodlot_id
+                           UNION ALL
+                           SELECT   max(sm.id) AS id,
+                                    sm.location_dest_id AS location_id,
+                                    sm.product_id,
+                                    sm.prodlot_id,
+                                    sum(sm.product_qty /uo.factor) AS qty
+                           FROM stock_move AS sm
+                           LEFT JOIN stock_location sl
+                           ON (sl.id = sm.location_dest_id)
+                           LEFT JOIN product_uom uo
+                           ON (uo.id=sm.product_uom)
+                           WHERE sm.state = 'done' AND sm.location_id != sm.location_dest_id
+                           GROUP BY sm.location_dest_id, sm.product_id, sm.product_uom, sm.prodlot_id
+                    ) AS report
+                    GROUP BY location_id, product_id, prodlot_id
+                    HAVING sum(qty) > 0)
         """)
 
-ReportStockReal()
+wms_report_stock_available()
 
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
